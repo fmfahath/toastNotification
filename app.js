42865745b1e7bb2c59e810c66488775548117336
fmfahath <fmfahath.dev@gmail.com>
2023-12-08 20:02:47 +0530

    fontawesme  icon added, css added+

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const  toastBoxEl = document.getElementById('toast-box');
-let successMsg = "Successfully Submitted";
-let errorMsg = "System Error";
-let invalidMsg = 'PLease, Check the input';
+let successMsg = '<i class="fa-solid fa-circle-check"></i>Successfully Submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>System Error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invaid input';
 
 function showToast(msg){
     let toastDivEl = document.createElement('div');
     toastDivEl.classList.add('toast-show');
     toastDivEl.innerHTML = msg;
+    
+ 
+
+    if(msg === successMsg){
+        toastDivEl.classList.add('success');
+    }
+    else if(msg === errorMsg){
+        toastDivEl.classList.add('error');
+    }
+    else{
+        toastDivEl.classList.add('invalid');
+    }
+  
+
     toastBoxEl.appendChild(toastDivEl);
+    
+
+console.log('finished');
 }
